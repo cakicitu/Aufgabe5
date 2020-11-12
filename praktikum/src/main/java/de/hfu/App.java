49161f02d0690e6eefbdffe253a1cd5c823b346a
Türker Cakici <tuerkercakici@web.de>
2020-11-12 16:12:06 +0100

    ignore target

---
+++ praktikum/src/main/java/de/hfu/App.java
@@ -0,0 +1,34 @@
+package de.hfu;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class App 
+{
+    public static void main ( String[] args )throws IOException
+    {
+    	System.out.println("Bitte gewuenschte Kleinbuchstaben eingeben"); 
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+	    String str = br.readLine(); 
+	    String sentence = ""; 
+	    for(int i=0;i<str.length();i++) 
+	    { 
+	     if(Character.isUpperCase(str.charAt(i))==true) 
+	     { 
+	      char ch2= (char)(str.charAt(i)+32); 
+	      sentence = sentence + ch2; 
+	     } 
+	     else if(Character.isLowerCase(str.charAt(i))==true) 
+	     { 
+	      char ch2= (char)(str.charAt(i)-32); 
+	      sentence = sentence + ch2; 
+	     } 
+	     else 
+	     sentence= sentence + str.charAt(i); 
+
+	    } 
+	    System.out.println(sentence); 
+
+    }
+}
